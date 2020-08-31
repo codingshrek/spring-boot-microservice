bbfe6527d3afdd7b3db62748906a3117224a26d0
AKASH Akash <akash.akash@soprasteria.com>
2020-08-31 13:34:01 +0530

    Added Junit tests

--- microservices/composite-service/src/test/java/com/microservice/composite/product/CompositeServiceApplicationTests.java
+++ microservices/composite-service/src/test/java/com/microservice/composite/product/CompositeServiceApplicationTests.java
@@ -1,13 +1,103 @@
 package com.microservice.composite.product;
 
+import com.microservice.api.core.product.Product;
+import com.microservice.api.core.recommendation.Recommendation;
+import com.microservice.api.core.review.Review;
+import com.microservice.composite.product.services.ProductCompositeIntegration;
+import com.microservice.util.exceptions.InvalidInputException;
+import com.microservice.util.exceptions.NotFoundException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+//import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import java.util.Collections;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CompositeServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
+
+    private static final int PRODUCT_ID_OK = 1;
+    private static final int PRODUCT_ID_NOT_FOUND = 2;
+    private static final int PRODUCT_ID_INVALID = 3;
+
+
+    @MockBean
+    private ProductCompositeIntegration integrationMockBean;
+
+    @Autowired
+    WebTestClient client;
+
+    @BeforeEach
+    public void setUp() {
+        when(integrationMockBean.getProduct(PRODUCT_ID_OK)).
+                thenReturn(new Product(PRODUCT_ID_OK, "name", 1, "mock-address"));
+        when(integrationMockBean.getRecommendations(PRODUCT_ID_OK)).
+                thenReturn(Collections.singletonList(new Recommendation(PRODUCT_ID_OK, 1, "author", 1, "content", "mock address")));
+        when(integrationMockBean.getReviews(PRODUCT_ID_OK)).
+                thenReturn(Collections.singletonList(new Review(PRODUCT_ID_OK, 1, "author", "subject", "content", "mock address")));
+
+        when(integrationMockBean.getProduct(PRODUCT_ID_NOT_FOUND)).thenThrow(new NotFoundException("NOT FOUND: " + PRODUCT_ID_NOT_FOUND));
+
+        when(integrationMockBean.getProduct(PRODUCT_ID_INVALID)).thenThrow(new InvalidInputException("INVALID: " + PRODUCT_ID_INVALID));
+    }
+
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+	public void getProductById(){
+    	client.get()
+				.uri("/product-composite/"+PRODUCT_ID_OK)
+				.accept(APPLICATION_JSON)
+				.exchange()
+				.expectStatus().isOk()
+				.expectHeader().contentType(APPLICATION_JSON)
+				.expectBody()
+				.jsonPath("$.productId").isEqualTo(PRODUCT_ID_OK)
+				.jsonPath("$.recommendations.length()").isEqualTo(1)
+				.jsonPath("$.reviews.length()").isEqualTo(1);
+
+
 	}
+
+
+    @Test
+    public void getProductNotFound() {
+
+        client.get()
+                .uri("/product-composite/" + PRODUCT_ID_NOT_FOUND)
+                .accept(APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectHeader().contentType(APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.path").isEqualTo("/product-composite/" + PRODUCT_ID_NOT_FOUND)
+                .jsonPath("$.message").isEqualTo("NOT FOUND: " + PRODUCT_ID_NOT_FOUND);
+    }
+
+    @Test
+    public void getProductInvalidInput() {
+
+        client.get()
+                .uri("/product-composite/" + PRODUCT_ID_INVALID)
+                .accept(APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isEqualTo(UNPROCESSABLE_ENTITY)
+                .expectHeader().contentType(APPLICATION_JSON)
+                .expectBody()
+                .jsonPath("$.path").isEqualTo("/product-composite/" + PRODUCT_ID_INVALID)
+                .jsonPath("$.message").isEqualTo("INVALID: " + PRODUCT_ID_INVALID);
+    }
 
 }
